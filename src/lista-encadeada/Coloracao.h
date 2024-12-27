4e6b3320db06419c9b348655659dc206560269a0
Karine Gomes <karine.gomes@estudante.ufjf.br>
2024-12-27 16:40:58 -0300

    Add files via upload

---
+++ src/lista-encadeada/Coloracao.h
@@ -0,0 +1,16 @@
+//
+// Created by gomes on 27/12/2024.
+//
+
+
+#ifndef COLORACAO_H
+#define COLORACAO_H
+
+enum class Coloracao {
+    SEMCOR = -1,
+    AZUL = 0,
+    VERDE = 1,
+    AMARELO = 2,
+    VERMELHO = 3
+};
+#endif //COLORACAO_H
