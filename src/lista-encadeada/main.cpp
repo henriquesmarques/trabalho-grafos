1a74462e5ad9cb4ad4fb8998e8e71ec9e8e3bfb0
Karine Gomes <karine.gomes@estudante.ufjf.br>
2025-01-04 14:31:37 -0300

    Add files via upload

--- src/lista-encadeada/main.cpp
+++ src/lista-encadeada/main.cpp
@@ -20,16 +20,46 @@ int main() {
     else
         cout << "Nao eh direcionado." << endl;
 
-    int n = g.nConexo();
-    if (n > 0)
-        cout << "Eh " << n << "-conexo." << endl;
+    if (g.ehConexo())
+        cout << "Eh conexo." << endl;
     else
         cout << "Nao eh conexo." << endl;
-
     if (g.ehArvore())
         cout << "Eh arvore." << endl;
     else
         cout << "Nao eh arvore." << endl;
 
+
+    cout<<endl<<endl;
+    if (g.verticePonderado())
+        cout<< "Vertice Ponderada." << endl;
+    else
+        cout << "Nao eh vertice ponderada." << endl;
+
+    cout<<"Grau do grafo: " << g.getGrau()<<endl;
+
+    if (g.ehCompleto())
+        cout << "Eh completo." << endl;
+    else
+        cout << "Nao eh completo." << endl;
+
+    if (g.ehBipartido())
+        cout << "Eh Bipartido." << endl;
+    else
+        cout << "Nao eh Bipartido." << endl;
+
+    cout << endl << "FUNCAO ARTICULACAO" << endl;
+    if (g.possuiArticulacao())
+        cout << "Possui articulacao." << endl;
+    else
+        cout << "Nao possui articulacao." << endl;
+
+    cout << endl << "FUNCAO PONTE" << endl;
+    if (g.possuiPonte())
+        cout<< "Possui Ponte." << endl;
+    else
+        cout << "Nao possui Ponte." << endl;
+
     return 0;
 }
+
